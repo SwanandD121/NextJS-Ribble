d39306b49d88b45c2f0ccd2c00447a54a21ae847
Swanand Deshpande <swananddeshpande121@gmail.com>
2023-06-25 16:34:01 +0530

    First Commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-const user = g.model('User', {
+const User = g.model('User', {
   name: g.string().length({ min: 2, max: 20}),
   email: g.string().unique(),
   avatarUrl: g.url(),
